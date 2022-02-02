64d06bfc3c4c0326a8c8ee1624c2d96066db2198
Henric Johansson <s2107575@student.hb.se>
2022-02-02 17:09:11 +0100

    skrivit "hej"

--- Digital Caddie/Program.cs
+++ Digital Caddie/Program.cs
@@ -7,6 +7,7 @@ namespace Digital_Caddie
         static void Main(string[] args)
         {
             Console.WriteLine("Hejsan allihopa");
+            Console.WriteLine("hej");
 
         }
     }
