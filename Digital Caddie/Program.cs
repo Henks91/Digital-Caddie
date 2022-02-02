38fef66225005e0f99a073bb24d69be8c80b2002
user <user@DESKTOP-D8JENEV>
2022-02-02 15:43:20 +0100

    Add project files.

---
+++ Digital Caddie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Digital_Caddie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
