7f15cc50a1a2a5a3c70e34626c70ff585f5438e1
fredriklinh <98946093+fredriklinh@users.noreply.github.com>
2022-02-03 15:45:37 +0100

    Test

--- Digital Caddie/Fiddes kladd.cs
+++ Digital Caddie/Fiddes kladd.cs
@@ -6,7 +6,7 @@ namespace Digital_Caddie
     {
         static void Testning()
         {
-            Console.WriteLine("Välkommen Fidde");
+            Console.WriteLine("Hej fredrik");
         }
     }
 }
