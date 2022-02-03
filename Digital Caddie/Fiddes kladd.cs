923ce9fea44291189919092b9ef57c39ffaa4c1f
Fredrik Linhardt <s2105691@student.hb.se>
2022-02-03 15:08:45 +0100

    Test

--- Digital Caddie/Fiddes kladd.cs
+++ Digital Caddie/Fiddes kladd.cs
@@ -6,8 +6,8 @@ namespace Digital_Caddie
     {
         static void Testning()
         {
-            Console.WriteLine("Välkommen Fidde");
-            Console.WriteLine("hej");
+            Console.WriteLine("Hej Henke ooch Carl");
+            
         }
     }
 }
