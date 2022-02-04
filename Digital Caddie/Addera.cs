345005465f92e49a9fea1104f5dc55101e0e4517
Henric Johansson <s2107575@student.hb.se>
2022-02-04 15:34:52 +0100

    Lagt till fullständiga koden för Kennel

--- Digital Caddie/Addera.cs
+++ Digital Caddie/Addera.cs
@@ -34,15 +34,48 @@ namespace Digital_Caddie
         }
         public static void RegistreraTest()
         {
-            Bag a = new Bag(); //skapa ett kennelobjekt, i temporara variabeln "a"
+            Kennel a = new Kennel(); //skapa ett kennelobjekt, i temporara variabeln "a"
+            Hund A = new Hund();
             a.namn = "Ringgirdens taxar";
             a.adress = "Kriksvangen 4";
             a.agare = "Hanna Persson";
-            a.hundar = new string[2]; //skapa kennelns hundlista (godtycklig langd 2 tillsvidare) 
+            a.hundar = new string[3]; //skapa kennelns hundlista (godtycklig langd 2 tillsvidare)
             a.hundar[0] = "Maja";
             a.hundar[1] = "Brutus";
-            BagRegister[0] = a; //lagg till kenneln i register-listan, pi forsta platsen
-        }
+            a.hundar[2] = "Pudel";
+            kennelRegister[0] = a; //lagg till kenneln i register-listan, pi forsta platsen 
+
+            Kennel b = new Kennel();
+            b.namn = "Hagalunds hundar";
+            b.adress = "Svegvagen 8";
+            b.agare = "Annette Ahl";
+
+            b.hundar = new string[2];
+            b.hundar[0] = "Jack";
+            b.hundar[1] = "Sack";
+            kennelRegister[1] = b;
+
+            Kennel c = new Kennel();
+            c.namn = "Brukshundsuppfodaren";
+            c.adress = "Skogsvagen 13";
+            c.agare = "Jonas Herksson";
+
+            c.hundar = new string[2];
+            c.hundar[0] = "Findus";
+            c.hundar[1] = "Felix";
+            kennelRegister[2] = c;
+
+            Console.Write("Utskrift av kennelRegister:");
+            for (int i = 0; i < kennelRegister.Length; i++) //loopa igenom hela registret                                                                 
+            {
+                Console.WriteLine(i + " Namn: " + kennelRegister[i].namn + "\nAdress: " +
+                    kennelRegister[i].adress + "\nAgare: " + kennelRegister[i].agare);
+                Console.Write("Hundar: ");
+                foreach (string hund in kennelRegister[i].hundar) //loopa igenom kennelns hundlista 
+
+
+                    Console.Write(hund + ", ");
+            }
     }
     
     
