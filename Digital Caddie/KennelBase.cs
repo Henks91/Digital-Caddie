8cb8063429290d86a8bd3605ab500a8c98721160
Henric Johansson <s2107575@student.hb.se>
2022-02-06 14:11:10 +0100

    Nytt inlagt på Kennel

---
+++ Digital Caddie/KennelBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Digital_Caddie
+{
+    internal class KennelBase
+    {
+
+        //public string[] hundar;
+        public Hund[] hundar; //men har vill antagligen att en hund ska kunna ha fler egna attribut, se klassen Hund
+    }
+}
