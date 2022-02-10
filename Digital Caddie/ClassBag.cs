58e12a0901498f88277aecaf915099a65f707ca4
fredriklinh <98946093+fredriklinh@users.noreply.github.com>
2022-02-10 14:06:59 +0100

    Lade till den av annas kod för att addera en plats i listan för att skapa ny bag

--- Digital Caddie/ClassBag.cs
+++ Digital Caddie/ClassBag.cs
@@ -7,7 +7,7 @@ namespace Digital_Caddie
     public class ClassBag
     {
         public string namnBag;
-
+        public static ClassBag[] bagLista = new ClassBag[0];
         ClassKlubba klubblista;
     
     
@@ -17,21 +17,21 @@ namespace Digital_Caddie
 
             //int antalBag = 0;
             
-            ClassBag[] baglista = new ClassBag[5];
+            ClassBag[] bag = new ClassBag[bagLista.Length + 1]; // adderar en plats för en nya bag
             ClassBag klubba = new ClassBag();
 
-            Console.WriteLine("Vad vill du att denna bagen ska heta? ");
+            Console.WriteLine("Vad vill du att din bag ska heta?: ");
             klubba.namnBag = Console.ReadLine();
             //Console.WriteLine("Hur många bags vill du skapa? ");
             //antalBag = int.Parse(Console.ReadLine());
             //Console.WriteLine("Vad vill du att denna bagen ska heta? ");
             //string namnBag = Console.ReadLine();
 
-            for (int b = 0; b <= 1; b++)
+            for (int i = 0; i < bagLista.Length; i++) // taget från Annas Wather kod där hon adderar en plats i listan för att skapa väder information
             {
 
 
-                baglista[b] = klubba;
+                bag[i] = bagLista[i];
 
                 ClassKlubba.Test();
                 break;
@@ -39,7 +39,7 @@ namespace Digital_Caddie
             }      
             
             Console.WriteLine(klubba.namnBag);
-              foreach (ClassBag m in baglista)
+              foreach (ClassBag m in bag)
                 {  
                 Console.WriteLine(m);
                 break;
